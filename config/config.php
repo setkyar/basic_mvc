d43e83721af514a695db66129b8f1d29b2bb8f9b
Set Kyar Wa Lar <setkyar16@gmail.com>
2016-11-30 00:21:18 +0630

    URL passing with router

--- config/config.php
+++ config/config.php
@@ -1,3 +1,21 @@
 <?php 
 
 Config::set('site_name', 'Your Site Name');
+
+Config::set('languages', ['en', 'fr']);
+
+
+// Routes . Route name => method prefix
+Config::set('routes', [
+	'default'	=> '',			//Default URI
+	'admin'		=> 'admin_',		//Default Admin URI
+]);
+
+//Set Default Route
+Config::set('default_route', 'default');
+//Set default Language
+Config::set('default_language', 'en');
+//Set default Controller
+Config::set('default_controller', 'pages');
+//Set default Method
+Config::set('default_action', 'index');
