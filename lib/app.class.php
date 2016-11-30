06afb06116d69db1cd0e357942ece4bb7d66f096
Set Kyar Wa Lar <setkyar16@gmail.com>
2016-11-30 13:06:50 +0630

    Add view

--- lib/app.class.php
+++ lib/app.class.php
@@ -19,10 +19,19 @@ class App {
 		//Calling controller's method
 		$controller_object = new $controller_class();
 		if (method_exists($controller_object, $controller_method)) {
-			$result = $controller_object->$controller_method();
+			//Controller's action may return a very path
+			$view_path 		= $controller_object->$controller_method();
+			$view_object 	= new View($controller_object->getData(), $view_path);
+			$content 		= $view_object->render();
 		} else {
 			throw new Exception('Method ' . $controller_method . ' of class ' . $controller_class . ' does not exit');
 		}
+
+		$layout = self::$router->getRoute();
+		$layout_path = VIEWS_PATH.DS.$layout. '.html';
+		$layout_view_object = new View(compact('content'), $layout_path);
+
+		echo $layout_view_object->render();
 	}
 
 }
